ea28865a0a8816c5f0fdb2dc2e23223500abad80
emanmohamed1710 <ema.emam1710@gmail.com>
2026-04-13 22:07:33 +0200

    first commit correct3

--- src/test/java/Ex2.java
+++ src/test/java/Ex2.java
@@ -2,7 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,16 +18,11 @@ public class Ex2 {
     @BeforeClass
     public void setup(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-
-            driver = new ChromeDriver(options);        }
-        else {
-            driver = new FirefoxDriver();
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver() ;
         }
         driver.get(" https://demo.guru99.com/test/login.html");
 
